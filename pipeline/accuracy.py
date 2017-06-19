9dd8f239dd49a55ba0a85ffe0f650e5d7743fcc4
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-06-18 22:10:58 -0300

    creating a random classifier

--- pipeline/accuracy.py
+++ pipeline/accuracy.py
@@ -1,3 +1,23 @@
+import random
+# Class that defines a classifier
+class MyClassifier():
+
+    # Method to train the classifier
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        return self
+
+    # Method to predict the data test
+    def predict(self, X_test):
+        predictions = []
+
+        for row in X_test:
+            label = random.choice(self.y_train)
+            predictions.append(label)
+
+        return predictions
+
 # import a data set
 from sklearn import datasets
 iris = datasets.load_iris()
@@ -10,8 +30,7 @@ y = iris.target
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
 
-from sklearn import tree
-classifier = tree.DecisionTreeClassifier()
+classifier = MyClassifier()
 
 # training the classifier
 classifier = classifier.fit(X_train, y_train)
